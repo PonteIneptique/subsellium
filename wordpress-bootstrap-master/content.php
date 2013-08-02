94c68113755d98ae088433d8910c08d4e01de88c
Clérice Thibault <leponteineptique@gmail.com>
2013-08-03 00:10:34 +0100

    Long titre accepte - HTML

--- wordpress-bootstrap-master/content.php
+++ wordpress-bootstrap-master/content.php
@@ -1,13 +1,16 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> class="post" role="article">
   
-  <header>
-    <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-	<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'wpbs-featured' ); ?></a>
-	<time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?=the_time('d')?><legend><?=the_time('M');?></legend></time>
-	<div class="coms"><?=comments_number( "0", "1", "%" ); ?><legend><?=comments_number( "echo", "echo", "echos" ); ?></legend> </div>
-	<div class="author" role="author"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php the_author_meta( 'display_name' ); ?>"><?=get_avatar( get_the_author_meta('user_email'), 40 );?></a></div>
-  </header> <!-- end article header -->
 
+	<header>
+		<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+
+		<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>" class="article-banner">
+			<?php the_post_thumbnail( 'wpbs-featured' ); ?>
+			<time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?=the_time('d')?><legend><?=the_time('M');?></legend></time>
+			<div class="coms"><?=comments_number( "0", "1", "%" ); ?><legend><?=comments_number( "echo", "echo", "echos" ); ?></legend> </div>
+			<div class="author" role="author" title="<?php the_author_meta( 'display_name' ); ?>"><?=get_avatar( get_the_author_meta('user_email'), 40 );?></div>
+		</a>
+	</header> <!-- end article header -->
   <section class="post_content clearfix">
     <?php the_content( __("Read more &raquo;","bonestheme") ); ?>
   </section> <!-- end article section -->
--- wordpress-bootstrap-master/header.php
+++ wordpress-bootstrap-master/header.php
@@ -9,7 +9,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		
+		<link rel="shortcut icon" type="image/png" href="./wp-ressources/favicon.png" />
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 				
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
